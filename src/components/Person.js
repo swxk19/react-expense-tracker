da8b0eb5b97db857fa1c2b2dae6f3f0903809ebc
swxk19 <s.wxk19@gmail.com>
2022-05-10 22:25:46 +0800

    2

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,8 +3,8 @@ const Person = ({person}) => {
   return (
     <div className = 'person'>
     <h4>{person.name}</h4>
-    <p>Paid for: ${(person.paidFor).toFixed(2)}</p>
-    <p>Owe: ${(person.owe).toFixed(2)}</p>
+    <p>Paid for: ${(person.paidFor)}</p>
+    <p>Owe: ${(person.owe)}</p>
     </div>
   )
 }
